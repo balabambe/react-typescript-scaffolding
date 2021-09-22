103275b96a5aed71ba57785021a94cab8ff85f10
Bambe <balabambe@gmail.com>
2021-09-22 18:30:14 +0800

    Breadcrumbs fix.

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -6,10 +6,13 @@ import routes from '@router/router';
 
 const Breadcrumbs: React.FC = () => {
   const match = matchRoutes(routes, window.location.pathname);
-  const filter = match.filter((matchRoute) => !matchRoute.route.hideBreadcrumb);
+  const filter = match.filter((matchRoute, index) => !matchRoute.route.hideBreadcrumb && index !== 0);
   return (
     <>
       <ul className="breadcumbs">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
         {filter.map((item) => (
           <li key={item.match.url}>
             <Link to={item.match.url}>{item.route.name}</Link>
