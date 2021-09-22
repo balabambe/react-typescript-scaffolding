05086a106af6e4e2c4273e1c99646488233b5297
Bambe <balabambe@gmail.com>
2021-09-22 18:30:47 +0800

    Mainlayout for navbar.

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { RouteConfigComponentProps } from 'react-router-config';
 
 import Breadcrumbs from '@components/Breadcrumbs';
+import Navbar from '@components/Navbar';
 import RouterOutlet from '@components/RouterOutlet';
 
-const MainLayout: React.FC<RouteConfigComponentProps> = ({route}) =>  (
-    <>
-      <h1>MainLayout</h1>
-      <Breadcrumbs />
-      <RouterOutlet route={route} />
-    </>
-  )
-;
+const MainLayout: React.FC<RouteConfigComponentProps> = ({ route }) => (
+  <>
+    <Navbar renderSecondLevel />
+    <h1>MainLayout</h1>
+    <Breadcrumbs />
+    <RouterOutlet route={route} />
+  </>
+);
 export default MainLayout;
