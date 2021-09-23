204af03cbcb800cf8cc76d7cdc9236306cfdbfd7
Bambe <balabambe@gmail.com>
2021-09-23 16:47:25 +0800

    Redux examples.

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { RouteConfigComponentProps } from 'react-router-config';
 
 import RouterOutlet from '@components/RouterOutlet';
+import { actions as appActions, selectors as appSelectors } from '@store/app';
 
-const Books: React.FC<RouteConfigComponentProps> = ({route}) =>  (
+const Books: React.FC<RouteConfigComponentProps> = ({route}) =>  {
+  const selector = useSelector(appSelectors.getLoadingStatus);
+  const dispatch = useDispatch();
+  const clickme = () => {
+    if (selector) {
+      dispatch(appActions.actionAppLoadingEnd());
+    } else {
+      dispatch(appActions.actionAppLoadingStart());
+    }
+  };
+  return(
     <>
       <h1>Books</h1>
+      {selector && 'isLoading'}
+      {!selector && 'isNotLoading'}
+      <br />
+      <button onClick={() => clickme()} type="button">Click me!</button>
       <RouterOutlet route={route} />
     </>
-  );
+  );};
 export default Books;
