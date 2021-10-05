c088fa52865eaa7b2bfddddcabc9a6c67d62f90f
Bambe <balabambe@gmail.com>
2021-10-05 18:14:48 +0800

    Breadcrumbs styling.

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -4,18 +4,22 @@ import { Link } from 'react-router-dom';
 
 import routes from '@router/router';
 
+import './Breadcrumbs.scope.scss';
+
 const Breadcrumbs: React.FC = () => {
   const match = matchRoutes(routes, window.location.pathname);
   const filter = match.filter((matchRoute, index) => !matchRoute.route.hideBreadcrumb && index !== 0);
   return (
     <>
-      <ul className="breadcumbs">
+      <ul className="breadcrumbs">
         <li>
           <Link to="/">Home</Link>
         </li>
-        {filter.map((item) => (
+        {filter.map((item, index) => (
           <li key={item.match.url}>
-            <Link to={item.match.url}>{item.route.name}</Link>
+            {filter.length - 1 !== index
+              ? <Link to={item.match.url}>{item.route.name}</Link>
+              : <span>{item.route.name}</span>}
           </li>
         ))}
       </ul>
