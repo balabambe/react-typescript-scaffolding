73f22b44f59df5180f5371e47436474296c55318
Bambe <balabambe@gmail.com>
2021-10-05 15:59:08 +0800

    Navbar extract child component.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,52 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import Icons from '@components/Icons';
-import routes from '@router/router';
+import routes, { TypeRoutes } from '@router/router';
 
 import './Navbar.scoped.scss';
 
 type TypeNavBar = {
   renderSecondLevel?: boolean,
 }
+
+type TypeNavBarUnionRoutes = TypeNavBar & TypeRoutes;
+
+const RenderParentLink: React.FC<TypeNavBarUnionRoutes> = (props) => {
+  const { path, icon, name, routes: children, renderSecondLevel } = props;
+  if (!children || !renderSecondLevel) {
+    return <>
+      <NavLink to={path}>
+        {icon && <i><Icons color="inherit" iconString={icon?.iconName} /></i>}
+        <span>{name}</span>
+      </NavLink>
+    </>;
+  }
+  return <>
+    <p>
+      {icon && <i><Icons color="inherit" iconString={icon?.iconName} /></i>}
+      <span>{name}</span>
+    </p>
+  </>;
+};
+
+RenderParentLink.defaultProps = {
+  renderSecondLevel: false,
+};
+
+const RenderChildLink: React.FC<TypeRoutes> = (props) => {
+  const { routes: children } = props;
+  if (!children) {
+    return <></>;
+  }
+  return <>
+    <ul className="child">
+      {children.map((second) => (second &&
+        <li key={second.path}>
+          <NavLink to={second.path}>{second.name}</NavLink>
+        </li>))}
+    </ul>
+  </>;
+};
 
 const Navbar: React.FC<TypeNavBar> = ({renderSecondLevel}) => {
   const filter = routes.filter((route) => !route.hideNav);
@@ -17,16 +56,8 @@ const Navbar: React.FC<TypeNavBar> = ({renderSecondLevel}) => {
       <ul className="parent">
         {filter.map((item) => (
             <li key={item.path}>
-              {item.icon && <Icons iconString={item.icon?.iconName} />}
-              <NavLink to={item.path}>{item.name}</NavLink>
-              {item?.routes && renderSecondLevel && <>
-                <ul className="child">
-                  {item.routes.map((second) => (second &&
-                    <li key={second.path}>
-                      <NavLink to={second.path}>{second.name}</NavLink>
-                    </li>))}
-                </ul>
-              </>}
+              <RenderParentLink {...item} renderSecondLevel={renderSecondLevel} />
+              {renderSecondLevel && <RenderChildLink {...item} />}
             </li>
           ))}
       </ul>
