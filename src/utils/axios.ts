fce87c91d067986cba7ca01637df8fd97c28ad2d
Bambe <balabambe@gmail.com>
2021-10-06 11:55:46 +0800

    Adjust axios.

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosPromise } from 'axios';
+import axios, { AxiosError, AxiosPromise, AxiosRequestConfig, AxiosResponse } from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
@@ -17,26 +17,26 @@ const apiRequest = (opts = {}, headers = {}): AxiosPromise =>
   });
 
 axiosInstance.interceptors.request.use(
-  (config) => {
+  (config: AxiosRequestConfig) => {
     if (config.method === 'get' && config.data !== undefined) {
       // eslint-disable-next-line no-param-reassign
       config.params = config.data;
     }
     return config;
   },
-  (error) => {
+  (error: AxiosError) => {
     throw error;
   }
 );
 
 axiosInstance.interceptors.response.use(
-  (response) => response.data,
-  (error) => {
-    const { code } = error.response.data;
+  (response: AxiosResponse) => response.data,
+  (error: AxiosError) => {
+    const { code } = error;
     if (code === '400401' && window.location.pathname !== '/login') {
       // Can be done with redirect page or clean credentials.
     }
-    throw error.response;
+    throw error;
   }
 );
 
