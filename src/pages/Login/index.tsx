66c63fb036a8425c473b0456e8ae72083efcfee2
Bambe <balabambe@gmail.com>
2021-10-25 10:43:29 +0800

    Login form fields generate by dynamic variable.

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,29 +3,38 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 
 import './Login.scoped.scss';
 
+const FormFields = {
+  USERNAME: 'username',
+  PASSWORD: 'password',
+} as const;
+
 interface ILoginInfo {
-  username: string;
-  password: string;
+  [FormFields.USERNAME]: string;
+  [FormFields.PASSWORD]: string;
 }
 
 const Login: React.FC = () => {
   const { register, handleSubmit } = useForm<ILoginInfo>();
-  const onSubmit: SubmitHandler<ILoginInfo> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<ILoginInfo> = (data: ILoginInfo) => {
+    console.log(data);
+  };
 
-  return <form onSubmit={handleSubmit(onSubmit)}>
-    <div className="login-block">
-      <label htmlFor="username">
-        <span>Username:</span>
-        <input {...register('username')} />
-      </label>
-      <label htmlFor="password">
-        <span>Password:</span>
-        <input {...register('password')} />
-      </label>
-      <input type="reset" value="Reset"/>
-      <button type="submit">Login</button>
-    </div>
-  </form>;
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="login-block">
+        {Object.values(FormFields).map((item) => (
+          <label htmlFor={item} key={item}>
+            <span>{item}:</span>
+            <input id={item} {...register(item)} />
+          </label>
+        ))}
+        <div className="btn-block">
+          <input type="reset" value="Reset" />
+          <button type="submit">Login</button>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
