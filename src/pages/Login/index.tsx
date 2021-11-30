c72eee76d2fc7029af3620fdf68e915d4ba3d57c
balabambe <balabambe@gmail.com>
2021-11-30 19:01:32 +0800

    Login layout.

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Button, Form, InputGroup } from 'react-bootstrap';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useHistory } from 'react-router-dom';
 
+import Icons from '@components/Icons';
 import './Login.scoped.scss';
 
 const FormFields = {
@@ -14,6 +16,21 @@ interface ILoginInfo {
   [FormFields.PASSWORD]: string;
 }
 
+const FormFieldsType = {
+  [FormFields.USERNAME]: 'text',
+  [FormFields.PASSWORD]: 'password',
+} as const;
+
+const FormFieldsIcon = {
+  [FormFields.USERNAME]: 'user',
+  [FormFields.PASSWORD]: 'key',
+} as const;
+
+const FormFieldsPlaceholderText = {
+  [FormFields.USERNAME]: 'Enter Username',
+  [FormFields.PASSWORD]: 'Enter Password',
+} as const;
+
 const Login: React.FC = () => {
   const history = useHistory();
   const { register, handleSubmit } = useForm<ILoginInfo>();
@@ -22,22 +39,31 @@ const Login: React.FC = () => {
   };
 
   return (
-    <form className="login-wrapper" onSubmit={handleSubmit(onSubmit)}>
+    <Form className="login-wrapper shadow" onSubmit={handleSubmit(onSubmit)}>
       <div className="login-block">
         <h1>Account Login</h1>
-        {Object.values(FormFields).map((item) => {
-          const capitalize = `${item[0].toUpperCase()}${item.slice(1)}`;
-          return (
-            <label htmlFor={item} key={item}>
-              <input id={item} placeholder={capitalize} {...register(item)} />
-            </label>
-          );
-        })}
+        {Object.values(FormFields).map((item) => (
+          <InputGroup className="mb-3" key={item}>
+            <InputGroup.Text>
+              <Icons iconString={FormFieldsIcon[item]} />
+            </InputGroup.Text>
+            <Form.Control
+              id={item}
+              placeholder={FormFieldsPlaceholderText[item]}
+              type={FormFieldsType[item]}
+              {...register(item)} />
+          </InputGroup>
+        ))}
         <div className="btn-block">
-          <button type="submit">Login</button>
+          <Button
+            className="px-5 shadow-sm"
+            size="lg"
+            type="submit"
+            variant="primary"
+          >Login</Button>
         </div>
       </div>
-    </form>
+    </Form>
   );
 };
 
